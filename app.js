e348f7208e30535c3577f9c0c4f3e9452ee894b9
Tolu Phillips <phillipsclaire27@gmail.com>
2025-05-21 18:15:00 +0100

    Task One

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert("Hello world!")
+let day = "wednesday"
+
+console.log(day)
+
+let position = "3"
+
+console.log(position)
+
+//let output = "today is wednesday '.' it is the 3rd day of the week"
+
+
+
+
+//console.log(output)
+
+console.log("today is " + day + "."  +   "it is the " + position + "rd day of the week") 
