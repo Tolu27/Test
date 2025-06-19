f97b08766d767c09803461d54ebeb8eedddbc730
Tolu Phillips <phillipsclaire27@gmail.com>
2025-06-19 15:34:48 +0100

    Task Odd Numbers

---
+++ Loops.js
@@ -0,0 +1,5 @@
+let car = 1  //starting point
+while(car <=10)(  //condition to stop
+    console.log(car + "car")
+    car = car + 1  //how to iterate
+)
--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-const triangleside1 = 9
-const triangleside2 = 7
-const triangleside3 = 8
-
-if(triangleside1 === triangleside2 && triangleside3)(
-    console.log("Equilateral Triangle")
-)
-else if(triangleside1 === triangleside2 || triangleside2 === triangleside3 || triangleside1 === triangleside3)(
-    console.log("Isoceles Triangle")
-)
-    else(
-        console.log("Scalene Triangle")
-    )
-
+ Let car = 1
+ 
