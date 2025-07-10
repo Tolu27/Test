bc083edba525753ea19043d99fe976a5a7a35bc2
Tolu Phillips <phillipsclaire27@gmail.com>
2025-07-10 22:25:42 +0100

    Descending array

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-//Print all even numbers from 0 - 100
-
-for (let i = 1; i <= 100; i++){
-    if (i % 2 === 0){
-        console.log(i)
-    }
-}
+let digits = [10,11,15,22,19]
+digits.sort((a, b)=> b - a)
+console.log(digits)
