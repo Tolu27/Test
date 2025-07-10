bcba85b5e965d3c29f8de26909b83cdafe325191
Tolu Phillips <phillipsclaire27@gmail.com>
2025-07-10 22:14:35 +0100

    Sort Alphabets in array

--- Array.js
+++ Array.js
@@ -1,9 +1,4 @@
-const number = [1, 2, 3, 4, 5, 6, 7]
-let count = 0
-let sum = 0
-let num = (number.length)
-while(count < num){
-    sum += number[count]
-    count += 1
-}
-console.log(sum)
+let fruits = ["pineapple", "orange", "lemon", "guava"]
+fruits.sort()
+
+console.log(fruits)
