62bb3012d3f38fc2f8816134d0f576ed2850a226
Tolu Phillips <phillipsclaire27@gmail.com>
2025-07-13 20:05:55 +0100

    Multiplication

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 function MultiplicationTable(number){
     let table = []
     let times = 1
-    while (times < 13){
+    while (times <13){
         let result = (number * times)
         times += 1
         table.push(result)
     }
-    console.log(table)
+    let count = 1
+    for (let i = 0; i <= table.length -1; i++){
+        console.log(number + "*" + count + "=" + table[i])
+        count +=1
+    }
 }
 MultiplicationTable(5)
